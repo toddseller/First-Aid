b13da964ffc0199171b619cee8bba976f3b01823
Todd Seller <me@toddseller.com>
2016-04-25 11:10:19 -0700

    Update sidekiq.rb config file

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,7 @@
 Sidekiq.configure_server do |config|
-  config.redis = { url: 'redis://redistogo:bc5aa9cc8fb40e422757e989a9f47d27@lab.redistogo.com:9952/' }
+  config.redis = { url: ENV['REDISTOGO_URL'] }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: 'redis://redistogo:bc5aa9cc8fb40e422757e989a9f47d27@lab.redistogo.com:9952/' }
+  config.redis = { url: ENV['REDISTOGO_URL'] }
 end

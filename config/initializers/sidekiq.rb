fdc0e025671feb2a7fce555b6ff6e22f097896bc
Todd Seller <me@toddseller.com>
2016-04-25 14:57:59 -0700

    mod procfile

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,9 @@
-# Sidekiq.configure_server do |config|
-#   config.redis = { url: ENV['REDISTOGO_URL'] }
-# end
+require 'sidekiq'
 
-# Sidekiq.configure_client do |config|
-#   config.redis = { url: ENV['REDISTOGO_URL'] }
-# end
+Sidekiq.configure_client do |config|
+  config.redis = { :size => 1 }
+end
+
+Sidekiq.configure_server do |config|
+  config.redis = { :size => 2 }
+end
